8eccc7bf0449e80c432e2f76d07326a5f55effff
Vlad_Mudryy <vladyslav.mudryy@gmail.com>
2022-08-23 05:01:41 +0300

    init commit

---
+++ src/services/MarvelService.js
@@ -0,0 +1,38 @@
+
+
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=61ed5f1ce2d3288622bbb5abe0ce0123';
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = async () => {
+        const res = this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`)
+        return res.data.results.map(this._transformCharacter)
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`)
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
+}
+
+export default MarvelService;
