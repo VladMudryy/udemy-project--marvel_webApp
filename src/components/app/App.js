ee2d04159974f077658cace97adaeb253e6b97c2
Vald <example@mail.com>
2022-09-12 04:08:10 +0300

    add 404Page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,6 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
-import {MainPage, ComicsPage} from '../pages';
+import {MainPage, ComicsPage, Page404} from '../pages';
 import AppHeader from "../appHeader/AppHeader";
 
 const App = () => {
@@ -13,6 +13,7 @@ const App = () => {
                     <Routes>
                         <Route path="/" element={<MainPage/>}/>
                         <Route path="/comics" element={<ComicsPage/>}/>
+                        <Route path="*" element={<Page404/>}/>
                     </Routes>
                 </main>
             </div>
--- src/components/comicsList/ComicsList.js
+++ src/components/comicsList/ComicsList.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import Spinner from '../spinner/Spinner';
 
@@ -41,11 +42,11 @@ const ComicsList = () => {
         const items = comicsList.map(item => {
             return (
                 <li key={item.id} className="comics__item">
-                    <a href="#">
+                    <Link to={`/comics/${item.id}`}>
                         <img src={item.thumbnail} alt={item.title} className="comics__item-img"/>
                         <div className="comics__item-name">{item.title}</div>
                         <div className="comics__item-price">{item.price}</div>
-                    </a>
+                    </Link>
                 </li>
             )
         })
